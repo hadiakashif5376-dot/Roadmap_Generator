570b27a9db66cded7bf9c0f6e650c163d038fcf0
hadiakashif5376-dot <hadiakashif5376@gmail.com>
2026-09-01 19:18:23 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,8 @@
-import os
 import streamlit as st
 from groq import Groq
 
+GROQ_API_KEY = st.secrets["GROQ_API_KEY"]
 
-# ==========================================
-# CONFIGURATION
-# ==========================================
-
-GROQ_API_KEY = os.environ.get("GROQ_API_KEY")
 MODEL_NAME = "openai/gpt-oss-120b"
 
 client = Groq(api_key=GROQ_API_KEY)
